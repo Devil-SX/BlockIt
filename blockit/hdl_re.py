3a70ec6674bbb4c8182a0ccbbb9ace7a6acbafee
DevilXXL <987249586@qq.com>
2023-07-05 00:23:20 +0800

    例化

--- blockit/hdl_re.py
+++ blockit/hdl_re.py
@@ -8,15 +8,26 @@ import re
 module_name_re = r"module\s+(\w+)"
 # g0 module_name
 
-module_space_re = r"module\s+\w+(?:\s*#\s*\(([\s\S]*?)\))?\s*\(([\s\S]*?)\)\s*;"
-# g0 parameter_space, g1 port_space
-port_data_re = r"((?:input|output|inout) +(?:reg|wire)? *(?:\[[^:]+\:[^\]]+\])?) *([\w]+),? *(?:\/\/)?([^\n]*)"
-# g0 port_type, g1 port_name, g2 port_comment(maybe empty)
+
+module_space_re = (
+    r"module\s+\w+(?:\s*#\s*\(([\s\S]*?)\))?\s*\(([\s\S]*?)\)\s*;([\s\S]*?)endmodule"
+)
+# g0 parameter_space, g1 port_space, g2 body_space
+
+
+# para space
 parameter_re = (
     r"parameter\s+(?:integer\s+)?(?:\[[^:]+\:[^\]]+\] *)?(\w+)(?:\s*=\s*)?(\d*)"
 )
-
-# g0 parameter_name, g2 default_value(maybe empty)
+# ()g0 parameter_name, g2 default_value(maybe empty)
+port_data_re = r"((?:input|output|inout)(?: +reg|wire)? *(?:\[[^:]+\:[^\]]+\])?) *([\w]+),? *(?:\/\/)?([^\n]*)"
+# (port_sapace)g0 port_type, g1 port_name, g2 port_comment(maybe empty)
+comment_re = r"\/\/([\s\S]*?)\n"
+# g0 comment
+instance_re = r"(\w+)(?:\s*#\s*\([\s\S]*?\))?\s+(\w+)\s*\(([\s\S]*?)\)\s*;"
+# (body_space)g0 instance_module, g1 instance_name, g2 instance_space
+instance_port_re = r"\.(\w+)\(\s*([\s\S]*?)\s*\)\s*?(?=,|\s)"
+# (instance_space)g0 port_name, g1 siganl_name
 
 
 def get_module_name(content: str) -> str:
@@ -43,6 +54,25 @@ def get_ports(content: str) -> list[tuple[str, str, str]]:
     return port_data
 
 
+def get_instance(content: str) -> list[tuple[tuple[str, str], list[tuple[str, str]]]]:
+    """返回Verilog例化模块s名称、端口信号"""
+    body_space = re.search(module_space_re, content)[3]
+    body_space = re.sub(comment_re, "", body_space)  # 移除注释内容
+    instance_data = re.findall(instance_re, body_space)
+    instance_name_list = [(instance[0], instance[1]) for instance in instance_data]
+    instance_port_list = [
+        re.findall(instance_port_re, instance[2]) for instance in instance_data
+    ]
+    instance_port_list_fixed = [
+        [
+            (port_signal[0], re.sub(r"\[[\s\S]*\]", "", port_signal[1]))  # 移除切片获得信号名称
+            for port_signal in instance
+        ]
+        for instance in instance_port_list
+    ]
+    return list(zip(instance_name_list, instance_port_list_fixed))
+
+
 if __name__ == "__main__":
     file_path = "test_file.v"
     with open(file_path, "r") as f:
@@ -52,3 +82,6 @@ if __name__ == "__main__":
     print()
     print("module's ports are:")
     print(get_ports(content))
+    print()
+    print("module's instance are:")
+    print(get_instance(content))
--- blockit/test_file.v
+++ blockit/test_file.v
@@ -1,33 +1,48 @@
-module rram (
-  input clk,
-  input rst_n,
-  input [WIDTH-1:0] i_data,
-  input rw_n,
-  input [10:0] i_adr,
-  output reg [WIDTH-1:0] o_data
+module sparse_adder64 
+(
+  input [63:0] A,
+  input [63:0] B,
+  output [63:0] SUM,
+  output CO
 );
 
-reg[WIDTH-1:0] rram_reg [10:0];
+  // 64 = 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + (11-1)
+  wire carry_interconnect[10:0];
+  assign carry_interconnect[0] = 1'b0;
 
-integer  i;
-always @(posedge clk or rst_n) begin
-  if (!rst_n)
-    begin
-        for(i=0;i<2048;i=i+1)
-          begin
-            rram_reg[i] <= 0;
-          end
+  genvar i;
+  generate
+
+    for (i = 2; i <= 10; i=i+1) begin : adder_instance
+      select_adder #(.COUNT(i)) sel(
+        .A(A[end_calculate(i):start_calculate(i)]),
+        .B(B[end_calculate(i):start_calculate(i)]),
+        .Cin(carry_interconnect[i-2]),
+        .S(SUM[end_calculate(i):start_calculate(i)]),
+        .Cout(carry_interconnect[i-1])
+      );
     end
-  else
-    if (rw_n) 
-    // Read
-      begin
-        o_data <= rram_reg[i_adr];
-      end
-    else
-    // Write
-      begin
-        rram_reg[i_adr] <= i_data;
-      end
-end
+  endgenerate
+  
+    select_adder #(.COUNT(10)) sel(
+    .A(A[63:54]),
+    .B(B[63:54]),
+    .Cin(carry_interconnect[9]),
+    .S(SUM[63:54]),
+    .Cout(carry_interconnect[10])
+  );
+
+  assign CO = carry_interconnect[10];
+
+  function integer start_calculate;
+    input integer x;
+    start_calculate = x*(x-1)/2 -1;
+  endfunction
+
+  function integer end_calculate;
+    input integer x;
+    end_calculate = x*(x-1)/2 +x -2;
+  endfunction
+
 endmodule
+
